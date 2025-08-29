c97d5584e283ba08f2030eb7d5c0d594d03aeb34
aleynacelen <aleyna_celen@hotmail.com>
2025-08-29 12:06:01 +0300

    back to project

--- Eshop Projem/EShop - Backend bitmiş hali/EShop.Data/Abstract/IUnitOfWork.cs
+++ Eshop Projem/EShop - Backend bitmiş hali/EShop.Data/Abstract/IUnitOfWork.cs
@@ -8,4 +8,7 @@ public interface IUnitOfWork : IDisposable
     IGenericRepository<TEntity> GetRepository<TEntity>() where TEntity : class;
     int Save();
     Task<int> SaveAsync();
+    Task<int> SaveAsync();
+    public int MyProperty { get; set; }
+
 }
